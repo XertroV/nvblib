4b67a769a88b2e84ea0f8a323de1e617f00e7ba0
Max Kaye <m@xk.io>
2015-05-03 14:19:13 +1000

    add op_code_map

--- nvblib/__init__.py
+++ nvblib/__init__.py
@@ -6,6 +6,7 @@ from .instructions.delegate import DelegateVote
 from .instructions.empower import EmpowerVote
 from .instructions.mod_res import ModResolution
 from .instructions.comment import CommentNulldata
+from .instructions import OP_TRANSFER, OP_DELEGATE, OP_CREATE, OP_CAST, OP_EMPOWER, OP_MOD_RES, OP_NULL
 
 instruction_map = {
     'create': CreateNetwork,
@@ -14,7 +15,23 @@ instruction_map = {
     'mod_res': ModResolution,
     'empower': EmpowerVote,
     'comment': CommentNulldata,
+    'transfer': None,
 }
 
-def instruction_lookup(i):
-    return instruction_map.get(i)
+op_code_map = {
+    OP_CREATE: CreateNetwork,
+    OP_CAST: CastVote,
+    OP_DELEGATE: DelegateVote,
+    OP_MOD_RES: ModResolution,
+    OP_EMPOWER: EmpowerVote,
+    OP_TRANSFER: None,
+}
+
+def instruction_lookup(instruction_str):
+    return instruction_map.get(instruction_str)
+
+def op_code_lookup(op_code_byte):
+    return op_code_map[op_code_byte]
+
+def get_op(script_bytes):
+    return script_bytes[3]
